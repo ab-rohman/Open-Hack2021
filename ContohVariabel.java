6ffc714bef13d8243800b8d8ea0556efd64abc7c
FarizieFar <90998221+FarizieFar@users.noreply.github.com>
2021-10-12 09:41:38 +0700

    alfarizie

--- ContohVariabel.java
+++ ContohVariabel.java
@@ -8,7 +8,7 @@ public class ContohVariabel{
 		byte _umurSayaSekarang = 18;
 		double $ipk = 3.24, tinggi = 1.70;
 		System.out.println(salahSatuHobySayaAdalah);
-		System.out.println("Apakah pandai? " + isPandai);
+		System.out.println("Apakah kamu pandai? " + isPandai);
 		System.out.println("Jenis kelamin: " + jenisKelamin);
 		System.out.println("Umurku saat ini: " + _umurSayaSekarang);
 		System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
