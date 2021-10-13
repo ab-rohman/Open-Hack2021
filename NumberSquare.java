e81458bb82522d0db6ee6a22bcb228c58419d3b2
Nirosh Neminda <50820417+NiroNemi@users.noreply.github.com>
2021-10-14 00:35:13 +0530

    outside declaration

--- NumberSquare.java
+++ NumberSquare.java
@@ -10,11 +10,12 @@ import java.util.Scanner;
 public class NumberSquare {
 
     public static void main(String[] args) {
+        int i, j;
         Scanner input = new Scanner(System.in);
         System.out.print("Input Your Number : ");
         int number = input.nextInt();
-        for (int i = 0; i < number; i++) {
-            for (int j = 0; j < number; j++) {
+        for (i = 0; i < number; i++) {
+            for (j = 0; j < number; j++) {
                 if (i == 0 || i == (number - 1) || j == 0 || j == (number - 1)) {
                     System.out.print(number);
                 } else {
