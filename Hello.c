a81befc1a04397607bf81c0843b8ef3cd0c09054
Disara7 <87073596+Disara7@users.noreply.github.com>
2021-10-17 21:40:10 +0530

    Fixed an error
    Added a missing semicolon and edited the program to print the output in two separate lines.

--- Hello.c
+++ Hello.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("Hello, World!");
-   printf("This is Hacktoberfest 2021")
+   printf("Hello, World!\n");
+   printf("This is Hacktoberfest 2021\n");
    return 0;
 }
