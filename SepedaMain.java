4b4308d5f44ae279627f004f78a8132d1d3e767e
rizqi-ardiansyah <86498942+rizqi-ardiansyah@users.noreply.github.com>
2021-10-26 07:48:22 +0700

    Upload penerapan object pada java

---
+++ SepedaMain.java
@@ -0,0 +1,29 @@
+/**
+ *
+ * @author Rizqi Ardiansyah
+ */
+public class SepedaMain {
+    public static void main(String[]args){
+        Sepeda spd1 = new Sepeda();
+        Sepeda spd2 = new Sepeda();
+        SepedaGunung spd3 = new SepedaGunung();
+        
+        spd1.setMerek("United");
+        spd1.tambahKecepatan(10);
+        spd1.gantiGear(2);
+        spd1.cetakStatus();
+        
+        spd2.setMerek("Pacific");
+        spd2.tambahKecepatan(10);
+        spd2.gantiGear(2);
+        spd2.tambahKecepatan(10);
+        spd2.gantiGear(3);
+        spd2.cetakStatus();
+        
+        spd3.setMerek("Rocky Mountain");
+        spd3.tambahKecepatan(5);
+        spd3.gantiGear(7);
+        spd3.setTipeSuspensi("Gas suspension");
+        spd3.cetakStatus();
+    }
+}
