f7f2cf7fe3d4bf92afcd7e22c92f5de5b7d40d3b
vishvikkrishnan <44338064+vishvikkrishnan@users.noreply.github.com>
2021-10-31 19:56:43 -0400

    Delete simple_interest.py function
    Deleting simple_interest.py to reflect changes with the main branch in the remote repo.

--- simple_interest.py
+++
@@ -1,5 +0,0 @@
-#Calculate Simple Interest
-def calc_si(principal, term, interest_rate):
-    simple_interest = (principal * term * interest_rate) / 100
-    amount = principal + simple_interest
-    return "Simple interest is {:.2f} and total amount is {:.2f}".format(simple_interest, amount)
